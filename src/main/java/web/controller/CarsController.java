83a45934e5935d0c51c48fcdaf63bd71d485bbe9
kulabuha <kulabuha@inbox.ru>
2021-10-26 10:25:19 +0300

    2.2.2 add service2

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,18 +1,22 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class CarsController {
 
     private final Car car;
+
+    @Autowired
+    CarService carService;
 
     public CarsController(Car car) {
         this.car = car;
@@ -21,10 +25,7 @@ public class CarsController {
     @GetMapping(value = "cars")
     public String printWelcome(@RequestParam(value = "count", required = false) String count, ModelMap model) {
         List cars = car.getListCar();
-        if (count == null) {
-            count = Integer.toString(cars.size());
-        }
-        model.addAttribute("cars",cars.stream().limit(Integer.parseInt(count)).collect(Collectors.toList()));
+        model.addAttribute("cars",carService.filterCars(cars, count));
         return "cars";
     }
 
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,2 +1,18 @@
-package web.service;public class CarServiceImpl {
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Override
+    public List<Car> filterCars(List<Car> cars, String count) {
+        if (count == null) {
+            count = Integer.toString(cars.size());
+        }
+        return cars.stream().limit(Integer.parseInt(count)).collect(Collectors.toList());
+    }
 }
